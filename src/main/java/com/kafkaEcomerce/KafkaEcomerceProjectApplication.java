049b7c9b4a7a0d120a245cf26e21d1496101dc8f
JYOTIKUMARI PRADHAN <jyotikumaripradhan000@gmail.com>
2026-09-23 15:26:20 +0530

    initial commit

---
+++ src/main/java/com/kafkaEcomerce/KafkaEcomerceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kafkaEcomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaEcomerceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaEcomerceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafkaEcomerce/KafkaEcomerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaEcomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEcomerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
